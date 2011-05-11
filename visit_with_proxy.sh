0eccbf72b4382a73d7fc41a4a745992d8216bda0
Walter Zheng <zdwalter@gmail.com>
2011-05-11 23:15:54 +0800

    ok

--- visit_with_proxy.sh
+++ visit_with_proxy.sh
@@ -13,8 +13,9 @@ pid=$!
 
 seq 60 | while read i; do
 sleep 1;
-[ ! -e "/proc/$pid/" ] && echo "$proxy $i `ls -l /tmp/$screen.png | awk '{print $5}'`" > test.log && exit 0;
+[ ! -e "/proc/$pid/" ] && exit 0;
 done
 
-kill -9 $pid
+kill -9 $pid 
+echo "$proxy $i `ls -l /tmp/$screen.png | awk '{print $5}'`" > test.log
 cat test.log | while read h x size; do [ $size -gt 10000 ] && ./retwis.sh $h; done
