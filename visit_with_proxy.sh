14487820ffab0b5a938ea94eee00a61590faeb95
Walter Zheng <zdwalter@gmail.com>
2011-05-12 11:24:01 +0800

    update

--- visit_with_proxy.sh
+++ visit_with_proxy.sh
@@ -4,20 +4,8 @@ proxy=$1
 url="http://zdwalter.blogspot.com"
 
 echo -n $proxy
+export http_proxy=$proxy
+curl $url -o /tmp/$screen.html
 
-killall -9 Xvfb;
-killall -9 xvfb-run;
-echo > /tmp/$screen.png
-xvfb-run -a -f /tmp/$screen.xauthority --server-args="-screen $screen, 800x600x16" ./CutyCapt --url=$url --out=/tmp/$screen.png --http-proxy="http://$proxy" 
-pid=$!
-
-seq 20 | while read i; do
-sleep 1;
-echo $i;
-[ ! -e "/proc/$pid/" ] && exit 0;
-done
-
-kill -9 $pid 
-echo "$proxy i `ls -l /tmp/$screen.png | awk '{print $5}'`" > test.log
-cat test.log
-cat test.log | while read h x size; do [ $size -gt 10000 ] && ./retwis.sh $h; done
+size=`ls -l /tmp/$screen.html | awk '{print $5}'`
+[ $size -gt 10000 ] && ./retwis.sh $proxy
